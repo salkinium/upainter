4554179aa1abfb72be2b7cf72986c35d4078df55
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-07-10 23:08:42 +0200

    Add generic Surface class using PixelColor::Type.

--- surface.hpp
+++ surface.hpp
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "pixel_format.hpp"
 #include "pixel_buffer.hpp"
-#include "color.hpp"
+#include "pixel_color.hpp"
 #include <cstring>
 #include <algorithm>
 
@@ -15,21 +15,89 @@ namespace ges
 {
 
 template< PixelFormat Format >
-class Surface;
+class Surface
+{
+	friend class QDisplay;
+	using BufferType = typename PixelColor<Format>::Type;
+public:
+	using NativeColor = PixelColor<Format>;
+
+	template< uint16_t Width, uint16_t Height >
+	using Buffer = PixelBuffer<Width, Height, Format>;
+
+public:
+	Surface(uint8_t *const buffer, const uint16_t width, const uint16_t height) :
+		width(width), height(height), buffer((BufferType*)buffer)
+	{}
+
+	template< uint16_t Width, uint16_t Height >
+	Surface(PixelBuffer<Width, Height, Format> &buffer) :
+		Surface(buffer.getData(), Width, Height)
+	{}
+
+	uint16_t
+	getWidth() const
+	{ return width; }
+
+	uint16_t
+	getHeight() const
+	{ return height; }
+
+	static constexpr PixelFormat
+	getPixelFormat()
+	{ return Format; }
+
+	void
+	clear()
+	{
+		std::memset(buffer, 0, std::size_t(width) * height * sizeof(BufferType));
+	}
+
+	void
+	clear(NativeColor color)
+	{
+		std::fill_n(buffer, std::size_t(width) * height, color.getValue());
+	}
+
+	bool
+	setPixel(uint16_t x, uint16_t y, NativeColor color)
+	{
+		if (x < width and y < height)
+		{
+			buffer[y * width + x] = color.getValue();
+			return true;
+		}
+		return false;
+	}
+
+	bool
+	clearPixel(uint16_t x, uint16_t y)
+	{
+		return setPixel(x, y, NativeColor(0));
+	}
+
+	Color
+	getPixel(uint16_t x, uint16_t y) const
+	{
+		if (x < width and y < height)
+		{
+			return Color(NativeColor(buffer[y * width + x]));
+		}
+		return Color::Black;
+	}
+
+protected:
+	const uint16_t width;
+	const uint16_t height;
+	BufferType *const buffer;
+};
 
 } // namespace ges
 
 } // namespace modm
 
-#include "surface/surface_argb2.hpp"
-#include "surface/surface_argb4.hpp"
-#include "surface/surface_argb8.hpp"
-#include "surface/surface_argb1555.hpp"
+
 #include "surface/surface_l1.hpp"
 #include "surface/surface_l4.hpp"
-#include "surface/surface_l8.hpp"
-#include "surface/surface_rgb1.hpp"
-#include "surface/surface_rgb332.hpp"
-#include "surface/surface_rgb565.hpp"
 
 #endif // MODM_GES_SURFACE_HPP
