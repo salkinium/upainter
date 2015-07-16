d63dfcd0b508a9d5b398a1230145318fa8f51a10
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-07-17 01:43:03 +0200

    Fix color ordering.

--- color.cpp
+++ color.cpp
@@ -6,16 +6,16 @@ namespace modm
 namespace ges
 {
 
-constexpr Color Color::Black   = Color(0xff000000);
-constexpr Color Color::White   = Color(0xffffffff);
+constexpr Color Color::Black   = Color(0, 0, 0);
+constexpr Color Color::White   = Color(0xff, 0xff, 0xff);
 
-constexpr Color Color::Red     = Color(0xffff0000);
-constexpr Color Color::Green   = Color(0xff00ff00);
-constexpr Color Color::Blue    = Color(0xff0000ff);
+constexpr Color Color::Red     = Color(0xff, 0, 0);
+constexpr Color Color::Green   = Color(0, 0xff, 0);
+constexpr Color Color::Blue    = Color(0, 0, 0xff);
 
-constexpr Color Color::Yellow  = Color(0xffffff00);
-constexpr Color Color::Cyan    = Color(0xff00ffff);
-constexpr Color Color::Magenta = Color(0xffff00ff);
+constexpr Color Color::Yellow  = Color(0xff, 0xff, 0);
+constexpr Color Color::Cyan    = Color(0, 0xff, 0xff);
+constexpr Color Color::Magenta = Color(0xff, 0, 0xff);
 
 // ...
 
